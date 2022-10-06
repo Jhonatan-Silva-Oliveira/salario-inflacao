4f0d01b662f6d09e63f763fc620aec37d1d27b47
JhonatanSO <jhonerex2012@gmail.com>
2022-10-06 09:38:56 -0300

    adicionado nova branch e iniciado projeto

---
+++ index.js
@@ -0,0 +1,7 @@
+import ReadLine from "readline-sync";
+
+console.log("\nEscolha uma das alternativas: \n\n");
+
+console.log("1 - Listar os salários minímos de 2010 à 2020\n");
+console.log("2 - Listar o índice IPCA de 2010 à 2020\n");
+console.log("3 - Comparando entre o percentual de aumento salarial e o IPCAn\n\n")
